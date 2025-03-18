ecf2f3006aff4da31ccbab496efe656ba6429c6b
liuzack0505 <liu.zack0505@gmail.com>
2025-03-18 14:43:47 +0800

    Initial commit

---
+++ Lab_04.js
@@ -0,0 +1,10 @@
+// Todo 3 : Write some function to handle the pass value from HTML
+// for example, you may have following functions to handle the different actions
+
+// function onkeydown() {}
+
+// function onclick() {}
+
+// function calculate() {}
+
+console.log("Fighting!");
